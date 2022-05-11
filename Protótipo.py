9c29f3eecc2267bc9fae02fd76ff76bfff08735f
Matheus Henrique Da Silva <matheush5529@gmail.com>
2022-05-10 23:40:41 -0300

    Melhoramento
    Implementando ideias de melhoramento

--- Protótipo.py
+++ Protótipo.py
@@ -1,6 +1,10 @@
 from select import select
 
-print('''Gastos com seu Pet!
+print('''GASTO ANUAL COM SEU PET!
 [ 1 ] Gato 
 [ 2 ] Cachorro''')
 select = int(input('Qual o seu Pet?: '))
+if select == 1:
+    gatos = int(input('Qauntos gatos você tem?: '))
+    ração = float(input('Qual o valor da Ração: '))
+    rkg = int(input('Quantos KG é o pacote da Ração?: '))
