74faecaf82bf98045c5d466c9c3faa629290dfbd
Matheus Henrique Da Silva <matheush5529@gmail.com>
2022-05-15 12:57:27 -0300

    up. granulado dhigienico

--- Protótipo.py
+++ Protótipo.py
@@ -34,6 +34,18 @@ if higiene == 1:
         print('Gastando R${:.2f} no pacote de {}Kg, comprando {} por mês. você tem um gasto anual de R$:{:.2f}'.format(valor, kg, quantidade, c1))
     else:
         quantidade = int(input('Compra a cada quantos mese?: '))
-        c =  (12 / quantidade) *valor
+        c =  (12 / quantidade) * valor
         print('Gastando R$:{:.2f} a cada {} meses, o seu gasto anual com areia é de {:.2f}'.format(valor, quantidade, c))
 elif higiene == 2:
+    valor = float(input('Qual o preço Do saco de Granulado Higiênico?: '))
+    kg =  float(input('Quantos Kg tem o pacote?: '))
+    if kg <= 15:
+        quantidade = int(input('Quantos você compra por mês?: '))
+        c = (kg * quantidade) * 12
+        c1 =  c * valor
+        print('Gastando R$: {:.2f} no pacote de {}Kg, comprando {} por mês. Você tem um gasto anual de {:.2f}'.format(valor, kg, quantidade, c1))
+    else: 
+       quantidade = input('Compra a cada quantos meses?: ')
+       c = (12 / quantidade) * valor
+       print('Gastando R$:{:.2f} a cada {} meses, o seu gasto anual com Granulado Higiênico é de {:.2f}'.format(valor, quantidade, c))
+        
