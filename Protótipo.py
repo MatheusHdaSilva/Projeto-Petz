ffa5a1a1c3c736c054f040b0dd6706810a2f1677
Matheus Henrique Da Silva <matheush5529@gmail.com>
2022-05-15 14:37:42 -0300

    Termino da de Higiene de gatos

--- Protótipo.py
+++ Protótipo.py
@@ -48,4 +48,18 @@ elif higiene == 2:
        quantidade = int(input('Compra a cada quantos meses?: '))
        c = (12 / quantidade) * valor
        print('Gastando R$:{:.2f} a cada {} meses, o seu gasto anual com Granulado Higiênico é de {:.2f}'.format(valor, quantidade, c))
+else:
+    valor1 = float(input('Qual o valor da areia?: '))
+    kg1 = float(input('Quantos Kg tem a areia?: '))
+    valor2 = float(input('Qual o valor do Granulado Higiênico?:'))
+    kg2 = float(input('Quantos Kg tem o Granulado?: '))
+    print('''Selecione uma das opções abaixo!!
+    [ 1 ] Você compra varios pacotes ao mês?
+    [ 2 ] você estoca um número de pacotes para comprar a cada 2 meses ou mais?''')
+    select = int(input('Sua escolha: '))
+    if select == 1:
+        areia = int(input('Quantos pacotes de areia você compra?: '))
+        granulado = int(input('Quantos pacotes de granulado você compra?: '))
+        c =  (areia * valor1) * 12 + (granulado * valor2) * 12
+        print('Gastando {:.2f} em areia de Tantos Kg{} e {:.2f} em granulado higiênico de {}Kg. Você tem o gasto anual de {:.2f}'.format(valor1, kg1, valor2, kg2, c))
         
--- Testes.py
+++ Testes.py
@@ -1,14 +1,14 @@
 
-valor = float(input('Qual o preço Do saco de Granulado Higiênico?: '))
-kg =  float(input('Quantos Kg tem o pacote?: '))
-if kg <= 15:
-    quantidade = int(input('Quantos você compra por mês?: '))
-    c = (kg * quantidade) * 12
-    c1 =  c * valor
-    print('Gastando R$: {:.2f} no pacote de {}Kg, comprando {} por mês. Você tem um gasto anual de {:.2f}'.format(valor, kg, quantidade, c1))
-else: 
-    quantidade = int(input('Compra a cada quantos meses?: '))
-    c = (12 / quantidade) * valor
-    print('Gastando R$:{:.2f} a cada {} meses, o seu gasto anual com Granulado Higiênico é de {:.2f}'.format(valor, quantidade, c))
-        
-        
+valor1 = float(input('Qual o valor da areia?: '))
+kg1 = float(input('Quantos Kg tem a areia?: '))
+valor2 = float(input('Qual o valor do Granulado Higiênico?:'))
+kg2 = float(input('Quantos Kg tem o Granulado?: '))
+print('''Selecione uma das opções abaixo!!
+[ 1 ] Você compra varios pacotes ao mês?
+[ 2 ] você estoca um número de pacotes para comprar a cada 2 meses ou mais?''')
+select = int(input('Sua escolha: '))
+if select == 1:
+    areia = int(input('Quantos pacotes de areia você compra?: '))
+    granulado = int(input('Quantos pacotes de granulado você compra?: '))
+    c =  (areia * valor1) * 12 + (granulado * valor2) * 12
+    print(c)
